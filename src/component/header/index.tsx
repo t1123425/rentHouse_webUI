65518f68776387a2ffccbf9b077140f472cc70d6
tom yuan <t1123425@gmail.com>
2020-12-09 02:59:18 +0800

    update header style & content layout

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Input,Avatar } from 'antd';
 import Logo from '../../assets/img/zotsuLogo.png';
-const { Search } = Input;
+import AvatarSrc from '../../assets/img/avatar.png';
+import { CaretDownOutlined} from '@ant-design/icons';
+const { Search} = Input;
+
 const Header = () =>{
     return (
         <header className="headerMenu">
@@ -10,7 +13,14 @@ const Header = () =>{
             </div>
             {/* <p>header</p> */}
             <div className="searchWrap">
-                    <Search placeholder="input search text" style={{ width: 200 }} />
+                    <Search 
+                      placeholder="input search text"
+                      size="large" 
+                      style={{ width: 320,height:48 }} />
+            </div>
+            <div className="avatarIcon">
+                <Avatar src={AvatarSrc} />
+                <CaretDownOutlined style={{fontSize:12,color:'#BFBFBF',marginLeft:8}} />
             </div>
         </header>
     )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {Row, Col} from 'antd';
+import {Row, Button, Col} from 'antd';
 import Header from '../../component/header';
 import Map from '../../component/map';
-
 function Home() {
-    console.log(process.env.PUBLIC_URL);
+    // console.log(process.env.PUBLIC_URL);
   return (
     <div className="Home">
       < Header />
       <div className="mainContent">
            <Row>
-             <Col span={14}>list</Col>
-             <Col span={10}>
+             <Col span={12}>
+                <div className="filterContent">
+                   <div className="locationFilter">
+
+                   </div>
+                   <div className="priceAndEvaluate">
+                    <Button shape="round" size={'large'}>
+                      價格
+                    </Button>
+                    <Button shape="round" size={'large'}>
+                      區域評價
+                    </Button>
+                   </div>
+                </div>
+                <div className="houseListContent">
+                    <h2>
+                      共找到 678 間房屋
+                    </h2>
+                    <ul className="houseList">
+
+                    </ul>
+                </div>
+             </Col>
+             <Col span={12}>
                 <Map />  
              </Col> 
            </Row>
