c565f0b97d4ac63f16f2d9a60f2a835463d6e077
tom yuan <tomyuan@tomdeMacBook-Pro.local>
2020-12-09 16:02:27 +0800

    add houselist layout & filter dropdown & update layout

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Input,Avatar } from 'antd';
-import Logo from '../../assets/img/zotsuLogo.png';
+import Logo from '../../assets/img/logo_zotsu.png';
 import AvatarSrc from '../../assets/img/avatar.png';
 import { CaretDownOutlined} from '@ant-design/icons';
 const { Search} = Input;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
-import {Row, Button, Col} from 'antd';
+import {Row, Button, Menu, Dropdown,Col,Space} from 'antd';
 import Header from '../../component/header';
 import Map from '../../component/map';
+import HousePrev from '../../assets/img/houseDemo.jpeg'
+import { CaretDownOutlined} from '@ant-design/icons';
+// interface stringArray{
+//   array?:String[]
+// }
+
+const cityList = (
+    <Menu>
+      <Menu.Item>
+        台北市
+      </Menu.Item>
+      <Menu.Item>
+        新北市
+      </Menu.Item>
+    </Menu>
+)
+const districtList = (
+    <Menu>
+      <Menu.Item>
+        大同區
+      </Menu.Item>
+      <Menu.Item>
+        信義區
+      </Menu.Item>
+    </Menu>
+)
+
 function Home() {
     // console.log(process.env.PUBLIC_URL);
   return (
@@ -9,27 +36,95 @@ function Home() {
       < Header />
       <div className="mainContent">
            <Row>
-             <Col span={12}>
-                <div className="filterContent">
-                   <div className="locationFilter">
-
-                   </div>
+             <Col span={12} className="houseListWrap">
+                <div className="filterContent textLeft">
+                  <Space className="locationFilter">
+                      <Dropdown overlay={cityList}>
+                          <h1>
+                            台北市
+                            <CaretDownOutlined style={{fontSize:16,marginLeft:8}} />
+                          </h1>
+                      </Dropdown>
+                      <Dropdown overlay={districtList}>
+                          <h1>
+                            大同區
+                            <CaretDownOutlined style={{fontSize:16,marginLeft:8}} />
+                          </h1>
+                      </Dropdown>
+                  </Space>
+                   {/* <div className="locationFilter">
+                   </div> */}
                    <div className="priceAndEvaluate">
-                    <Button shape="round" size={'large'}>
+                    <Button shape="round">
                       價格
                     </Button>
-                    <Button shape="round" size={'large'}>
+                    <Button shape="round" style={{marginLeft:24}}>
                       區域評價
                     </Button>
                    </div>
                 </div>
-                <div className="houseListContent">
-                    <h2>
+                <div className="houseListContent textLeft">
+                    <h2 className="title">
                       共找到 678 間房屋
                     </h2>
-                    <ul className="houseList">
-
-                    </ul>
+                    <Row gutter={[16, 16]}>
+                      <Col span={8}>
+                         <a href="#" className="houseInfoBox">
+                           <div className="prevImg">
+                              <img src={HousePrev}  alt="prev"/>
+                           </div>
+                           <div className="houseInfo">
+                              <p>迪化商圈歸綏街3+1房 (入籍.家具可談)</p>
+                              <div className="priceBox">
+                                  <span>99,999</span>
+                                  <span>元/月</span>
+                              </div>
+                           </div>
+                         </a>
+                      </Col>
+                      <Col span={8}>
+                         <a href="#" className="houseInfoBox">
+                           <div className="prevImg">
+                              <img src={HousePrev}  alt="prev"/>
+                           </div>
+                           <div className="houseInfo">
+                              <p>北車捷運京站地下街便利舒適電梯全新大套房</p>
+                              <div className="priceBox">
+                                  <span>99,999</span>
+                                  <span>元/月</span>
+                              </div>
+                           </div>
+                         </a>
+                      </Col>
+                      <Col span={8}>
+                         <a href="#" className="houseInfoBox">
+                           <div className="prevImg">
+                              <img src={HousePrev}  alt="prev"/>
+                           </div>
+                           <div className="houseInfo">
+                              <p>迪化商圈歸綏街3+1房 (入籍.家具可談)</p>
+                              <div className="priceBox">
+                                  <span>99,999</span>
+                                  <span>元/月</span>
+                              </div>
+                           </div>
+                         </a>
+                      </Col>
+                      <Col span={8}>
+                         <a href="#" className="houseInfoBox">
+                           <div className="prevImg">
+                              <img src={HousePrev}  alt="prev"/>
+                           </div>
+                           <div className="houseInfo">
+                              <p>台北車站【五星飯店式】獨棟電梯~獨立套房</p>
+                              <div className="priceBox">
+                                  <span>99,999</span>
+                                  <span>元/月</span>
+                              </div>
+                           </div>
+                         </a>
+                      </Col>
+                    </Row>
                 </div>
              </Col>
              <Col span={12}>
