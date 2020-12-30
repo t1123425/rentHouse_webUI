b62d0213e0ce18a79937754890856a26bd4ebddb
tom yuan <tomyuan@tomdeMacBook-Pro.local>
2020-12-30 16:22:15 +0800

    update modalwithbtn

--- src/component/modal/modalwithbtn.tsx
+++ src/component/modal/modalwithbtn.tsx
@@ -6,17 +6,11 @@ interface PropObj {
 }
 
 function useOutsideAlerter(ref:any) {
-    console.log('ref',ref);
     useEffect(() => {
-        /**
-         * Alert if clicked on outside of element
-         */
         function handleClickOutside(event:any):void {
             if (ref.current && !ref.current.contains(event.target)) {
                 // alert("You clicked outside of me!");
                 console.log('outside');
-            }else{
-                console.log('inside');
             }
         }
 
@@ -31,16 +25,44 @@ function useOutsideAlerter(ref:any) {
 
 const ModalWithBtn = (prop:PropObj) => {
     const [show,setShow] = useState(false);
-    const wrapperRef = useRef(null);
-    const test =  useOutsideAlerter(wrapperRef);
+    let wrapperRef = useRef<HTMLDivElement>(null);
+    // let showStatus =  useOutsideAlerter(wrapperRef);
+    // console.log('t',test);
+
+    const handleshowhide = (e:any) => {
+        e.stopPropagation();
+        setShow(true);
+    }
+    useEffect(() => {
+        function handleClickOutside(event:any):void {
+            // if(wrapperRef){
+            //     
+            // }
+            if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+                    // alert("You clicked outside of me!");0.00
+                    console.log('outside');
+                    setShow(false);
+             }
+            // console.log('event.target',event.target);
+            // console.log('wrapperRef',wrapperRef.current.contains(event.target));
+        }
+
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [wrapperRef]);
     return (
-        <div className={'modalWrap '} ref={wrapperRef} >
-            <Button shape="round">
+        <div className={'modalWrap'} >
+            <Button shape="round" onClick={handleshowhide}>
                 {
                     prop.btnName
                 }
             </Button>
-            <div className={'modalContent '+(!show?'hide':'')}>
+            <div className={'modalContent '+(!show?'hide':'')} ref={wrapperRef}>
+                
                 <div className="priceRangeFilter">
                     <h2>區域中位數價格為 $20,000</h2>
                     <div className="priceRangeInputContent">
